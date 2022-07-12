e09f5adddfcbd63ef38f792c75a1efc3740b305d
Alejandro Avila <alejandroaperez1994g@gmail.com>
2022-07-12 14:58:54 +0100

    feat: :sparkles: added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import { useEffect, useState } from "react";
-
-import { products } from "./data/products";
-
-import Product from "./components/Product/Product";
-import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
 import Navbar from "./components/Navbar/Navbar";
 
 import "./App.css";
@@ -12,38 +7,9 @@ const initialShoppingCart =
   JSON.parse(localStorage.getItem("shoppingCart")) || [];
 
 function App() {
-  const [buyShoppingCart, setBuyShoppingCart] = useState(initialShoppingCart);
-
-  useEffect(() => {
-    localStorage.setItem("shoppingCart", JSON.stringify(buyShoppingCart));
-  }, [buyShoppingCart]);
-
   return (
     <div className="main__app">
-      <div className="main__products">
-        {/* <div className="products_list">
-          {products.map((item, index) => {
-            return (
-              <Product
-                key={index}
-                id={index}
-                title={item.title}
-                description={item.description}
-                valoration={item.valoration}
-                price={item.price}
-                image={item.image}
-                cart={buyShoppingCart}
-                setCart={setBuyShoppingCart}
-              />
-            );
-          })}
-        </div> */}
-      </div>
-      <div className="test" anchor="right">
-        <h1>Shopping Cart</h1>
-
-        {/* <ShoppingCart cart={buyShoppingCart} setCart={setBuyShoppingCart} /> */}
-      </div>
+      <Navbar />
     </div>
   );
 }
